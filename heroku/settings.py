7ab22cea4c42a3dc2e1599402462790691f96bcd
SunJoon-Lee <tjswns1002@gmail.com>
2019-06-01 00:32:12 +0900

    배포가즈아

--- heroku/settings.py
+++ heroku/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(3$a$)*3k7i2@i^h!4ohv4jy8rfq4#
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
